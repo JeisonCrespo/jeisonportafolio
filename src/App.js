cbe6914ac9159b8fbe1ffa8b60c5ff0d67993c70
Jeison Crespo <jeisoncrespo14@gmail.com>
2021-11-11 14:27:35 -0500

    commit prueba

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>hola como estas</p>
+        <p>hola mundo</p>
         <a
           className="App-link"
           href="https://reactjs.org"
